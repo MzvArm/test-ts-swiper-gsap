0f21b6a010a28193d561b648340ccd18c075a0e1
Morozov Artem <mz.artemkr@gmail.com>
2026-04-01 21:06:34 +0300

    Made base configuration

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import './main.scss'
+
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+});
+
+console.log("Hello! Swiper is ready.");
+
+console.log("Hello This is a Typescript project.");
+
